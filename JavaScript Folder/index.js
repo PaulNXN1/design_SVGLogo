be9a9a791eb59ab874d3757e9e011eac99ec7a0f
Paul Nixon <paulnxn1@gmail.com>
2023-05-24 19:22:46 -0400

    Added prompt questions

---
+++ JavaScript Folder/index.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+const questions = [
+   
+    {
+      type: 'list',
+      message: 'What is the shape of your choice?',
+      choices: ['circle', 'square' , 'triangle']
+    },
+
+    {
+      type: 'input',
+      message: 'Please select a color!',
+      name: 'color',
+    },
+
+    {
+      type: 'input',
+      message: 'What text would you like to add to your shape?',
+      name: 'text_input',
+    }
+
+   ]
